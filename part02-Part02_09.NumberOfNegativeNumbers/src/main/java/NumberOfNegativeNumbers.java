a179f776980feebcb30b7bf6fb43a903098aa13e
Dixita <69583355+dixitadas@users.noreply.github.com>
2024-02-05 15:34:29 +0530

    Add part02

---
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            }
+            if (input < 0) {
+                num = num + 1;
+            }
+        }
+        System.out.println("Number of negative numbers: " + num);
+    }
+}
